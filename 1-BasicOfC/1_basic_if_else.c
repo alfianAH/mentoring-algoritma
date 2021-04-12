f67b3bca6ce6e1bbb93fad5ee7c7ddd3519015ea
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-04-12 10:37:55 +0800

    Add basic of if else in C

---
+++ 1-BasicOfC/1_basic_if_else.c
@@ -0,0 +1,61 @@
+#include "stdio.h"
+#include "string.h"
+
+void inputChar();
+void inputString();
+
+int main(){
+    int number_integer = 123;
+    char character = 'a';
+    char string[] = "alphabet";
+
+    printf("If integer \n");
+
+    if(number_integer >= 1){
+        printf("Masuk if");
+    } else{
+        printf("Masuk else");
+    }
+
+//    printf("\n\nIf character\n");
+//    inputChar(character);
+//
+//    printf("\n\nIf string\n");
+//    inputString(string);
+
+    return 0;
+}
+
+// Fungsi untuk input char
+void inputChar(character){
+    char input;
+
+    printf("Masukkan char:");
+    scanf("%c", &input);
+    printf("Hasil input: %c\n", input);
+
+    // If else statement
+    if(input == character){
+        printf("Masuk if");
+    } else{
+        printf("Masuk else");
+    }
+}
+
+// Fungsi untuk input string
+void inputString(string){
+    char input[] = "";
+
+    printf("Masukkan char:");
+    scanf("%s", &input);
+    printf("Hasil input: %s\n", input);
+
+    // If else statement
+    if(strcmp(input, string) == 0){
+        printf("Masuk if");
+    } else if(strcmp(input, "kamu") == 0) {
+        printf("Masuk else if");
+    } else{
+        printf("Masuk else");
+    }
+}
